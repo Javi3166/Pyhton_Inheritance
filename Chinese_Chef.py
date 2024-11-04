f944257c8ac7c4d84a0ea660efff550ac5812fb1
Emilio <javi3166@yahoo.com>
2024-11-04 15:45:23 -0500

    Initial commit and finished code

---
+++ Chinese_Chef.py
@@ -0,0 +1,8 @@
+from Chef import Chef
+
+class Chinese_Chef(Chef):
+    def make_special_dish(self):
+        print("The chef makes orange chicken.")
+
+    def make_fried_rice(self):
+        print("The chef makes fried rice.")
---
+++ Main.py
@@ -0,0 +1,9 @@
+from Chef import Chef
+from Chinese_Chef import Chinese_Chef
+
+myChef = Chef()
+myChef.make_special_dish()
+
+myChineseChef = Chinese_Chef()
+myChineseChef.make_fried_rice()
+myChineseChef.make_special_dish()
